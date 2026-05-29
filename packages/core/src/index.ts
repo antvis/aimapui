6d88412803b2f0a6a88ab496739569ce4190ff99
lzxue <120635640@qq.com>
2026-05-29 14:48:23 +0800

    fix: 显式指定 AiMap index.tsx 文件后缀

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,7 +7,7 @@
 import './styles/tailwind.css';
 
 // 主入口组件
-export { AiMap } from './components/AiMap/index';
+export { AiMap } from './components/AiMap/index.tsx';
 export type { AiMapProps } from './components/AiMap/types';
 
 // Schema 类型
